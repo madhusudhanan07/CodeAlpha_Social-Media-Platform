35e2c661866b3288044f8df8658d72aa1c4ce0fd
madhusudhanan07 <madhusudhananna24cb@psnacet.edu.in>
2026-08-01 23:44:00 +0530

    feat: initialize Firebase app and auth service with environment variables

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1Ux2132wL9Ttkv4_VSV_udBZvfALeXdA",
-  authDomain: "codealphasocial-media-platform.firebaseapp.com",
-  projectId: "codealphasocial-media-platform",
-  storageBucket: "codealphasocial-media-platform.firebasestorage.app",
-  messagingSenderId: "14179781922",
-  appId: "1:14179781922:web:c4825b51ab54807e0ea97b",
-  measurementId: "G-JEL1KQFX9H"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
